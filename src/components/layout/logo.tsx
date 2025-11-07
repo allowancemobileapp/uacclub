3aba2593d5818d25f069a9ad86d428645a2d8527
como update <comoupdate@gmail.com>
2025-11-07 18:51:14 +0000

    let the site title show in the nav bar mobile view

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -5,7 +5,7 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2">
       <Wind className="h-7 w-7 text-primary" />
-      <span className="text-xl font-semibold tracking-tight font-headline hidden sm:block">
+      <span className="text-xl font-semibold tracking-tight font-headline">
         UAC
       </span>
     </Link>
